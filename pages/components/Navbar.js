23732c9d6dfe65048849c9360e8661a7d6e6f9f3
Rakesh Sadhukhan <rakeshvirat99@gmail.com>
2022-04-14 15:49:00 +0530

    navbar\

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -10,14 +10,14 @@ export default function Navbar() {
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    {/* <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
         <a className="nav-link active" aria-current="page" href="#">Home</a>
         <a className="nav-link" href="#">About</a>
-        <a className="nav-link" href="#">Gallery</a>
+        <Link href="https://merakesh99.blogspot.com/"><a className="nav-link" target='_blank'>Blog</a></Link>
         <a className="nav-link disabled">Projects</a>
       </div>
-    </div> */}
+    </div>
   </div>
 </nav>
 
